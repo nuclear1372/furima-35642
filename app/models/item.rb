666df7467141eaabd08b50562521b9715415e7ae
nuclear1372 <aks74um71@gmail.com>
2021-07-25 10:17:22 +0900

    Category.rbの作成
    ・Category.rbに関するアソシエーションとバリデーションの設定をする。
    ・new.html.erbを編集し、正しく表示されているかを確認する。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :status
+  belongs_to :category
   
   with_options numericality: {other_than: 0, message: "can't be blank"} do
     validates :prefecture_id
-    validates :status_id 
+    validates :status_id
+    validates :category_id 
   end
 end
