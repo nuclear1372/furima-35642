6babd01e7557c812a930b1697672efcd43d91b48
nuclear1372 <aks74um71@gmail.com>
2021-07-25 10:29:08 +0900

    days_to_ship.rbの作成
    ・days_to_ship.rbに関わるアソシエーションとバリデーションの記述をする。
    ・new.html.erbを編集し、正しく表示されるかを確認する。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :status
   belongs_to :category
+  belongs_to :days_to_ship
   
   with_options numericality: {other_than: 0, message: "can't be blank"} do
     validates :prefecture_id
     validates :status_id
-    validates :category_id 
+    validates :category_id
+    validates :days_to_ship_id
   end
 end
