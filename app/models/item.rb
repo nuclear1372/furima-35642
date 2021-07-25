7f8087328655426828ba5c2d9cb82de594dc63cf
nuclear1372 <aks74um71@gmail.com>
2021-07-25 09:54:21 +0900

    Prefecture.rbに関するアソシエーションとバリデーションの設定
    ・new.html.erbのフォーム部品を編集し、正しく表示されているかを確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,6 @@
 class ShippingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 end
