c424f68647198c3518b53a64c63f36b673a35759
nuclear1372 <aks74um71@gmail.com>
2021-07-26 20:11:15 +0900

    itemモデルのpriceカラムのバリデーションを追加
    保存可能な値を300~9999999までの範囲に設定する。

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,8 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
     validates :image
   end
+
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
