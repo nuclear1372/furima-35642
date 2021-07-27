1ce843695567db593c06bed64bb7d94aec875873
nuclear1372 <aks74um71@gmail.com>
2021-07-28 07:27:08 +0900

    ダミーデータの修正
    priceの値の生成範囲の下限を修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name               { Faker::Lorem.sentence }
     description        { Faker::Lorem.sentences }
-    price              { Faker::Number.between(from: 1, to: 9_999_999) }
+    price              { Faker::Number.between(from: 300, to: 9_999_999) }
     category_id        { Faker::Number.between(from: 1, to: 10) }
     status_id          { Faker::Number.between(from: 1, to: 6) }
     delivery_charge_id { Faker::Number.between(from: 1, to: 2) }
