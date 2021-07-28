123ec24b22be5c6e10800bde1c7acff2ad1f6703
nuclear1372 <aks74um71@gmail.com>
2021-07-28 19:04:44 +0900

    不要なアソシエーションを削除

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,7 +1,5 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
 
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 end
