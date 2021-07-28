19734c2dea21acb64842f244f91e3204a19393f9
nuclear1372 <aks74um71@gmail.com>
2021-07-29 07:23:09 +0900

    Rubocopによる自動修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,5 +1,5 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase
 
-  #validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  # validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 end
