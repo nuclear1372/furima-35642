63ed0a45244f79be79c2f8f7fe143216d2da207a
nuclear1372 <aks74um71@gmail.com>
2021-08-04 19:19:08 +0900

    モデルテストの実装
    Formオブジェクトのpurchase_shipping_addressのおけるテストコードの実装を行う。

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -7,20 +7,33 @@ class PurchaseShippingAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A[0][8-9][0][0-9]{8,8}\z/, message: "is invalid. Input only number"}
     validates :token
+    validates :phone_number, format: {with: /\A[0-9]{,11}\z/, message: "is invalid. Input only number"}
+    validates :user_id
+    validates :item_id
   end
-
+  validates :phone_number, format: {with: /\A[0-9]{10,}\z/, message: "is too short"}, if: :validates_length?
+  
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(
-      postal_code: postal_code, 
-      prefecture_id: prefecture_id, 
-      city: city, 
-      house_number: house_number, 
-      building_name: building_name, 
-      phone_number: phone_number, 
-      purchase_id: purchase.id
-    )
+    if purchase.id.nil?
+      @purchase_shipping_address.errors[:purchase_id] = "can't be blank"
+    else  
+      ShippingAddress.create(
+        postal_code: postal_code, 
+        prefecture_id: prefecture_id, 
+        city: city, 
+        house_number: house_number, 
+        building_name: building_name, 
+        phone_number: phone_number, 
+        purchase_id: purchase.id
+      )
+    end
+  end
+
+  def validates_length?
+    if phone_number.length <= 9
+      return true
+    end
   end
 end
---
+++ spec/factories/purchase_shipping_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchase_shipping_address do
+    token {"tok_" + Faker::Lorem.characters(number: 20)}
+    user_id {1}
+    item_id {1}
+    postal_code {"123-4567"}
+    prefecture_id {1}
+    city {"近江八幡市"}
+    house_number {"1-1-1"}
+    building_name {"レイクビュー近江"}
+    phone_number {"09012345678"}
+  end
+end
